3e14355381108a7ab6333911336fcd2d2ad7e444
davidlimonesast <david_limones@hotmail.com>
2023-03-01 14:22:48 -0700

    Nuevo Login

--- Login/main.js
+++ Login/main.js
@@ -1,15 +1,38 @@
-function login(){
-const username=document.getElementById('username')
-const password=document.getElementById('password')
-const button=document.getElementById('button')
+var email = document.forms['form']['email'];
+var password = document.forms['form']['password'];
 
-button.addEventListener('click', (e) => {
-    e.preventDefault()
-    const username1 = username.value;
-    const password1 = password.value;
-    console.log(username);
-    console.log(password);
+var email_error = document.getElementById('email_error');
+var pass_error = document.getElementById('pass_error');
 
-})
+email.addEventListener('textInput', email_Verify);
+password.addEventListener('textInput', email_Verify);
 
+function validated(){
+    if (email.value.length < 9){
+        email.style.border = "1px solid red";
+        email_error.style.display = "block";
+        email.focus();
+        return false;
+    }
+    if (password.value.length < 9){
+        password.style.border = "1px solid red";
+        pass_error.style.display = "block";
+        password.focus();
+        return false;
+    }
+}
+
+function email_Verify(){
+    if (email.value.length >=8){
+        email.style.border = "1px solid silver";
+        email_error.style.display = "none";
+        return true;
+    }
+}
+function pass_Verify(){
+    if (password.value.length >=5){
+        password.style.border = "1px solid silver";
+        pass_error.style.display = "none";
+        return true;
+    }
 }
